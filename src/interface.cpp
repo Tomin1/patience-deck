f96ad13490421c1be529aa6b62eed0ae824d9d6a
Tomi Leppänen <tomi@tomin.site>
2021-09-05 13:35:37 +0300

    Allow destroying callback after completing the action
    Signed-off-by: Tomi Leppänen <tomi@tomin.site>

--- src/interface.cpp
+++ src/interface.cpp
@@ -447,11 +447,15 @@ SCM Interface::delayedCall(SCM callback)
     scm_gc_protect_object(callback);
     engine->m_delayedCallTimer = new QTimer();
     QObject::connect(engine->m_delayedCallTimer, &QTimer::timeout, engine, [engine, callback] {
+        // The callback may setup another delayed call, set the current one to null already
         engine->m_delayedCallTimer->deleteLater();
         engine->m_delayedCallTimer = nullptr;
 
         if (engine->makeSCMCall(callback, nullptr, 0, nullptr))
             engine->endMove(true);
+    });
+    QObject::connect(engine->m_delayedCallTimer, &QObject::destroyed, engine, [callback] {
+        scm_gc_unprotect_object(callback);
     });
     engine->m_delayedCallTimer->start(DelayedCallDelay);
     return SCM_EOL;
