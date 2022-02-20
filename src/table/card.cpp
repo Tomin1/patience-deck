82df4ead546941bd10cd7f10b22d5cfd6180bd47
Tomi Leppänen <tomi@tomin.site>
2022-02-20 06:59:19 +0200

    Use qobject_cast instead of dynamic_cast
    Signed-off-by: Tomi Leppänen <tomi@tomin.site>

--- src/table/card.cpp
+++ src/table/card.cpp
@@ -1,6 +1,6 @@
 /*
  * Patience Deck is a collection of patience games.
- * Copyright (C) 2020-2021 Tomi Leppänen
+ * Copyright (C) 2020-2022 Tomi Leppänen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -242,7 +242,7 @@ void Card::mouseMoveEvent(QMouseEvent *event)
 
 Slot *Card::slot() const
 {
-    return dynamic_cast<Slot *>(parentItem());
+    return qobject_cast<Slot *>(parentItem());
 }
 
 bool Card::highlighted() const
