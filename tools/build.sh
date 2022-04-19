c12dbcabd26824810c7156a70041fe3bb7c70023
Tomi Leppänen <tomi@tomin.site>
2022-04-19 12:59:44 +0300

    Use sfdk in build.sh
    Use sfdk to build releases.
    
    Signed-off-by: Tomi Leppänen <tomi@tomin.site>

--- tools/build.sh
+++ tools/build.sh
@@ -14,14 +14,16 @@
 # OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+: "${SFDK:=$HOME/.local/opt/SailfishOS/bin/sfdk}"
+
 usage() {
-    2>&1 echo "Usage: $0 [--tag TAG] --dir DIRECTORY TARGET [TARGET...]"
+    2>&1 echo "Usage: $0 [--tag TAG] [--dir DIRECTORY] TARGET [TARGET...]"
     exit 1
 }
 
 clean() {
     git reset --hard HEAD
-    rm -f Makefile *.list
+    rm -f Makefile ./*.list
     find data/ qml/ src/ tools/ translations/ \( \
         -name Makefile -o \
         -name '*.list' -o \
@@ -36,10 +38,6 @@ clean() {
     pushd translations/
     git reset --hard HEAD
     popd
-}
-
-build() {
-    mb2 -t "$1" build -p -d $2
 }
 
 DIR=""
@@ -69,7 +67,7 @@ done
 
 TARGETS=$@
 
-if [ "$DIR" = "" ] || [[ "${#TARGETS[@]}" -eq 0 ]]
+if [[ "${#TARGETS[@]}" -eq 0 ]]
 then
     usage
 fi
@@ -86,7 +84,11 @@ fi
 for target in $TARGETS
 do
     echo "Building for $target"
-    build "$target" "$HARBOUR"
-    cp -v RPMS/*patience-deck-[0-9]*.rpm "$DIR/"
+    $SFDK -c "target=$target" build -p -d $HARBOUR
+    if [ "$DIR" != "" ]
+    then
+        cp -v "$($SFDK config --show | grep output-prefix \
+            | cut -d' ' -f3)/$target/"*patience-deck-[0-9]*.rpm "$DIR/"
+    fi
     clean
 done
