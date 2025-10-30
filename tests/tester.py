6d3e540e73ba81bbc1a92c67577fa6f94726a5f8
Tomi Leppänen <tomi@tomin.site>
2025-10-30 23:40:19 +0200

    Update test script with crc and version
    Check CRC in tests. Skip some false positive warnings in tests. Also
    drop --time as unused now that time can be read from --moves. It was
    hardcoded to "60" anyway.
    
    Signed-off-by: Tomi Leppänen <tomi@tomin.site>

--- tests/tester.py
+++ tests/tester.py
@@ -15,6 +15,7 @@
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import argparse
+import os
 import re
 import shlex
 import subprocess
@@ -31,7 +32,9 @@ RESULTS = [
     "OTHER", # 5
 ]
 
-COMMENT = re.compile(r"(?P<code>WON|INVAL|LOST|STUCK|NEW|OTHER)(?:, errors (?P<errors>\d+))?")
+COMMENT = re.compile(r"(?P<code>WON|INVAL|LOST|STUCK|NEW|OTHER)(?:, errors (?P<errors>\d+))?(?:, CRC (?P<crc>A\d+))?")
+CRC_LINE = re.compile(br"\[D\] [^ ]+:\d+ - CRC is (?P<crc>\d+)")
+CRC_FAIL = re.compile(br"\[W\] [^ ]+:\d+ - CRC failure after replaying, expected \d+ but got (?P<crc>\d+)")
 
 def result_text(code):
     if code < len(RESULTS):
@@ -44,37 +47,80 @@ def read_saved_states(filename):
     parser.add_argument('-g', '--game')
     parser.add_argument('-s', '--seed')
     parser.add_argument('-m', '--moves')
+    parser.add_argument('-c', '--crc')
     parser.add_argument('-o', '--options', default="")
+    parser.add_argument('-v', '--version')
     with open(filename, 'r') as file:
         for line in filter(lambda l: not l.startswith('#'), file):
             parts = line.partition('#')
             comment = COMMENT.search(parts[2])
             code = 0
             expected_errors = 0
+            expected_crc = None
             if comment:
                 code = result_codes[comment.group("code")]
                 if comment.group("errors"):
                     expected_errors = int(comment.group("errors"))
-            yield parser.parse_args(shlex.split(parts[0])[1:]), code, expected_errors
+                if comment.group("crc"):
+                    expected_crc = comment.group("crc")
+            yield parser.parse_args(shlex.split(parts[0])[1:]), code, expected_errors, expected_crc
 
-def run(test, expected_result, expected_errors):
+def filter_errors(line):
+    # Only critical and warning lines
+    if not line.startswith(b'[W] ') and not line.startswith(b'[C] '):
+        return False
+    # May happen but does not affect the result
+    if line.endswith(b'No window. Deleting texture later. This may leak fds!'):
+        return False
+    return True
+
+def filter_crc(line):
+    return bool(CRC_LINE.match(line) or CRC_FAIL.match(line))
+
+def get_crc_line(line, crc):
+    if line is not None:
+        match = CRC_LINE.match(line) or CRC_FAIL.match(line)
+        if match:
+            return match.group("crc")
+        assert False, "Not reached"
+    return crc
+
+def get_crc(lines):
+    crc_lines = filter(filter_crc, lines)
+    crc = get_crc_line(next(crc_lines, None), None)
+    crc = get_crc_line(next(crc_lines, None), crc)
+    return crc
+
+def run(test, expected_result, expected_errors, expected_crc):
     print(f"Testing with {test.game} (seed {test.seed})...", end="")
     sys.stdout.flush()
     command = ["patience-deck", "--test", "--game", test.game, "--seed", test.seed,
-               "--moves", test.moves, "--time", "60", "--options", test.options]
-    result = subprocess.run(command, timeout=TIMEOUT, capture_output=True)
+               "--moves", test.moves, "--options", test.options]
+    if test.crc is not None:
+        command.extend(["--crc", test.crc])
+    if test.version is not None:
+        command.extend(["--version", test.version])
+    if expected_crc is None:
+        expected_crc = test.crc
+    env = dict(os.environ)
+    env["QT_LOGGING_RULES"] = "site.tomin.patience.engine.recorder.crc=true"
+    result = subprocess.run(command, timeout=TIMEOUT, capture_output=True, env=env)
     errors = []
-    for line in filter(lambda l: l.startswith(b'[W] ') or l.startswith(b'[C] '), result.stderr.split(b'\n')):
+    for line in filter(filter_errors, result.stderr.split(b'\n')):
         errors.append(line.decode('UTF-8'))
-    if result.returncode == expected_result and len(errors) == expected_errors:
+    crc = get_crc(result.stderr.split(b'\n'))
+    if crc is not None:
+        crc = 'A' + crc.decode('UTF-8')
+    succeeded = result.returncode == expected_result and len(errors) == expected_errors and crc == expected_crc
+    if succeeded:
         status = "succeeded" if expected_errors == 0 else f"succeeded with {len(errors)} expected errors"
-        print(f" {status}, result {result_text(result.returncode)}")
+        print(f" {status}, result {result_text(result.returncode)} with crc {crc}")
     else:
         status = "FAILED" if errors == expected_errors else f"FAILED with {len(errors)} errors"
-        print(f" {status}, result {result_text(result.returncode)}, expected {result_text(expected_result)}")
+        print(f" {status}, result {result_text(result.returncode)}, expected {result_text(expected_result)}, crc {crc}, expected {expected_crc}")
     for line in errors:
         print("-->", line)
-    return result.returncode == expected_result and len(errors) == expected_errors
+    return succeeded
 
 def main():
     parser = argparse.ArgumentParser()
@@ -82,9 +128,9 @@ def main():
     args = parser.parse_args()
     count = 0
     successful = 0
-    for test, expected_result, expected_errors in read_saved_states(args.filename):
+    for test, expected_result, expected_errors, expected_crc in read_saved_states(args.filename):
         count += 1
-        if run(test, expected_result, expected_errors):
+        if run(test, expected_result, expected_errors, expected_crc):
             successful += 1
     if count == successful:
         print(f"All {count} tests passed!")
